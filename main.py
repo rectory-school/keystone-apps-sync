ae1d48b09acea6e8624d9d69ff9b2371d97edd17
Adam Peacock <adam@thepeacock.net>
2021-09-06 13:23:19 -0400

    Allow config on environment or CLI

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 
 import argparse
 import logging
+import os
 
 from managers import ParentManager, StudentManager, TeacherManager, CourseManager
 
@@ -16,28 +17,33 @@ def main():
     """Main entrypoint"""
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--student-file", default="ksPERMRECS.xml.json")
-    parser.add_argument("--teacher-file", default="ksTEACHERS.xml.json")
-    parser.add_argument("--course-file", default="ksCOURSES.xml.json")
-    parser.add_argument("--families-file", default="psFAMILIES.xml.json")
-    parser.add_argument("--api-root", required=True)
-    parser.add_argument("--username", required=True)
-    parser.add_argument("--password", required=True)
+    parser.add_argument("--student-file", default=os.environ.get("STUDENT_FILE", "data/ksPERMRECS.xml.json"))
+    parser.add_argument("--teacher-file", default=os.environ.get("TEACHER_FILE", "data/ksTEACHERS.xml.json"))
+    parser.add_argument("--course-file", default=os.environ.get("COURSE_FILE", "data/ksCOURSES.xml.json"))
+    parser.add_argument("--families-file", default=os.environ.get("FAMILIES_FILE", "data/ptFAMILIES.xml.json"))
+    parser.add_argument("--api-root", default=os.environ.get("API_ROOT"))
+    parser.add_argument("--username",  default=os.environ.get("USERNAME"))
+    parser.add_argument("--password",  default=os.environ.get("PASSWORD"))
 
     args = parser.parse_args()
     auth = (args.username, args.password)
 
-    students = StudentManager(args.api_root, auth=auth, ks_filename=args.student_file)
+    students = StudentManager(args.api_root, auth=auth,
+                              ks_filename=args.student_file)
     students.sync()
 
-    teachers = TeacherManager(args.api_root, auth=auth, ks_filename=args.teacher_file)
+    teachers = TeacherManager(args.api_root, auth=auth,
+                              ks_filename=args.teacher_file)
     teachers.sync()
 
-    courses = CourseManager(args.api_root, auth=auth, ks_filename=args.course_file)
+    courses = CourseManager(args.api_root, auth=auth,
+                            ks_filename=args.course_file)
     courses.sync()
 
-    parents = ParentManager(args.api_root, auth=auth, ks_filename=args.families_file)
+    parents = ParentManager(args.api_root, auth=auth,
+                            ks_filename=args.families_file)
     parents.sync()
+
 
 if __name__ == "__main__":
     main()
